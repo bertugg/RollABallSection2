ec9f65b79544ac7330210aa7dc0036e22dd657eb
bertugg <bertugbenim@gmail.com>
2020-10-27 12:04:04 +0300

    Prepared for Homework

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speedMultiplier = 4f;
+    public Joystick joystick;
 
     private float _movementHorizontal, _movementVertical;
     private Rigidbody _rigidbody;
+    
     
     // Awake is called as this initialize
     private void Awake()
@@ -25,16 +27,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            Debug.Log(Input.touches[0].position);
+        }
     }
 
     // FixedUpdate is called 60 times per sec.
     private void FixedUpdate()
     {
+        // Default movement with axis Inputs
         _movementHorizontal = Input.GetAxis("Horizontal") * speedMultiplier;
         _movementVertical = Input.GetAxis("Vertical") * speedMultiplier;
-        
+
         _rigidbody.AddForce(_movementHorizontal, 0,_movementVertical, ForceMode.Acceleration);
+        // Touch Position - Ball Position
     }
 
     private void OnCollisionEnter(Collision other)
